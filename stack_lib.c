ef22795289440116d49c8dc2771b96683ee8ad35
Nadinda <nadinda@nadinda.dev>
2022-10-22 12:24:53 +0200

    Delete unused code in stack_lib.c

--- stack_lib.c
+++ stack_lib.c
@@ -4,9 +4,7 @@
 long int factorial_recursive(int n);
 
 void push(int stack[], int val) {
-
 	int num_element = countElements(stack);
-	//printf("n: %d\n", num_element);
 	if (num_element >= STACK_SIZE) {
 		printf("Stack is full.\n");
 		exit(1);
@@ -14,8 +12,6 @@ void push(int stack[], int val) {
 	}
 
 	stack[num_element] = val;
-	//printf("push %d v: %d\n", num_element, val);
-	printf("v: %d\n", val);
 	printStack(stack);
 	return;
 }
@@ -28,11 +24,9 @@ int pop(int stack[]) {
 		exit(1);
 	}
 	num_element--;
-	//printf("The last value is:\n");
-	//printf("Value: %d\n", stack[num_element]);
+
 	int deleted_element = stack[num_element ];
 	stack[num_element ] = EMPTY;
-	//printf("pop %d %d\n", num_element, deleted_element);
 	printStack(stack);
 
 	return deleted_element;
@@ -49,7 +43,6 @@ int isEmpty(int stack[]) {
 int countElements(int stack[]) {
 	int i;
 	for (i = 0; i < STACK_SIZE; i++) {
-		//printf("count %d n: %d\n", i, stack[i]);
 		if (stack[i] == EMPTY)
 			break;
 	}
@@ -58,13 +51,11 @@ int countElements(int stack[]) {
 
 void printStack(int stack[]) {
 	int num_element = countElements(stack);
-	//printf("printStack, num: %d\n", num_element);
 	//if (num_element == 0)
 		//printf("Stack is empty.\n");
 	num_element--;
 	while ((num_element >= 0) && (stack[num_element] != EMPTY)) {
-		//printf("Parameter n: %d\n", num_element);
-		//printf("Return value of factRect(%d): %d\n", num_element, stack[num_element]);
+		printf("value: %d\n", stack[num_element]);
 		num_element--;
 	}
 }
@@ -74,4 +65,3 @@ void initStack(int stack[]) {
 		stack[i] = EMPTY;
 	}
 }
-
